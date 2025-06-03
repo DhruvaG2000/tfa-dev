0460388c1a9812fdaff383eae01ccbee4ac1bfc1
Kamlesh Gurudasani <kamlesh@ti.com>
2025-06-03 18:54:33 +0530

    changes to boot
    Change-Id: I2e2c6ea57f658eea9f70ef41f48a0a9037a9b239

--- plat/ti/k3low/board/am62lx/pm/soc_pm.mk
+++ plat/ti/k3low/board/am62lx/pm/soc_pm.mk
@@ -1,11 +1,11 @@
 PLAT_INCLUDES		+=	\
-				-I${PLAT_PATH}/board/am62l/pm/include/			\
+				-I${PLAT_PATH}/board/am62lx/pm/include/			\
 
 BL31_SOURCES		+=	\
-				${PLAT_PATH}/board/am62l/pm/clk_soc_hfosc0.c    \
-				${PLAT_PATH}/board/am62l/pm/clk_soc_lfosc0.c    \
-				${PLAT_PATH}/board/am62l/pm/clocks.c    \
-				${PLAT_PATH}/board/am62l/pm/devices.c    \
-				${PLAT_PATH}/board/am62l/pm/host_idx_mapping.c    \
+				${PLAT_PATH}/board/am62lx/pm/clk_soc_hfosc0.c    \
+				${PLAT_PATH}/board/am62lx/pm/clk_soc_lfosc0.c    \
+				${PLAT_PATH}/board/am62lx/pm/clocks.c    \
+				${PLAT_PATH}/board/am62lx/pm/devices.c    \
+				${PLAT_PATH}/board/am62lx/pm/host_idx_mapping.c    \
 
 
