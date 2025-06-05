582c2a42e6d6a89c799d98bc9abb7b51a3120fbd
Dhruva Gole <d-gole@ti.com>
2025-06-05 12:05:27 +0530

    feat(ti): add MMU regions for AM62L SoC
    Update the k3low bl31 platform setup to map required device regions
    (USART, GIC, GTC, and mailbox) in the MMU. This ensures that all
    necessary hardware blocks are accessible to the A53 cores on the
    AM62L SoC.
    Also use 4K aligned address sizes wherever applicable, and update the
    file header comment from "K3 SOC specific bl31_setup" to "k3low SoC
    specific bl31_setup" to accurately represent the platform specific
    nature of this file.
    
    Change-Id: I58209bc9c780db3e452b09c2c939bb0c47a63ed1
    Signed-off-by: Dhruva Gole <d-gole@ti.com>

--- plat/ti/k3low/include/platform_def.h
+++ plat/ti/k3low/include/platform_def.h
@@ -12,7 +12,12 @@
 #define TI_MAILBOX_TX_BASE		UL(0x44240000) /* TFA sending IPC messages to TIFS */
 #define TI_MAILBOX_RX_BASE		UL(0x44250000) /* TIFS sending IPC messages to A53 */
 
+/* We just need 512 bytes starting from TX/RX each, but simpler to just remap a 64K page */
+#define TI_MAILBOX_SIZE		UL(0x10000)
+
 #define WKUP_CTRL_MMR0_DEVICE_MANAGEMENT_BASE	(0x43050000UL)
+#define WKUP_CTRL_MMR0_DEVICE_MANAGEMENT_SIZE  (0x10000UL)
+
 #define WKUP_CTRL_MMR0_DEVICE_RESET_OFFSET	(0x4000UL)
 
 #endif /* PLATFORM_DEF_H */
